8d23bf797ac9404b1ae2034e53839bd6a5c742d9
Narissa Tsuboi <narissatsuboi@gmail.com>
2021-09-18 15:23:50 -0700

    Correct Happy Numbers problem

--- 3 - Fast and Slow Pointers/Happy Numbers (medium)/main.py
+++ 3 - Fast and Slow Pointers/Happy Numbers (medium)/main.py
@@ -16,13 +16,13 @@ Approach:
  a duplicate square sum has been reached. Else, its reached 1.
 """
 
+import unittest
+
 # Steps
 # 1. Calculate the square of the sums for pos/neg numbers
 # 2. Assign to slow and fast pointers
 # 3. Repeat until nums are the same.
 # 4. If never the same, happy number!
-
-import unittest
 
 
 def square_sums(num):
@@ -32,24 +32,35 @@ def square_sums(num):
     """
     sum_square_digits = 0  # accumulator for sum of the sq of all digits in num
     while num > 0:  # diminish num one digit at a time
-        digit = num%10  # mod 10 to get the digit
+        digit = num % 10  # mod 10 to get the digit
         sum_square_digits = digit * digit  # add the sq of the digits
         num //= 10
-
     return sum_square_digits
 
 
 def find_happy_number(num):
     slow, fast = num, num
+    print("Num=", num, end=' ')
     while True:
-        slow = find_happy_number(num)
-        fast = find_happy_number(find_happy_number(fast))
+        slow = square_sums(slow)
+        fast = square_sums(square_sums(fast))
+        print(slow, fast, "->", end='')
         if slow == fast:
             break
+    print()
     return slow == 1
+
+
+class TestHappyNumbers(unittest.TestCase):
+    def test_find_happy_number(self):
+        self.assertEqual(False, find_happy_number(0))
+        self.assertEqual(True, find_happy_number(1))
+        self.assertEqual(True, find_happy_number(23))
+        self.assertEqual(True, find_happy_number(2))
+        self.assertEqual(True, find_happy_number(24))
 
 
 if __name__ == '__main__':
     print("Happy Numbers")
-    square_sums(23)
-
+    # print(find_happy_number(23))
+    unittest.main(verbosity=2)
