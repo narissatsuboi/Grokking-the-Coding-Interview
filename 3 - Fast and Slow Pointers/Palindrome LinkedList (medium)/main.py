2a74b2f5ac2739f1743fd989cf1c03f08e6c34ae
Narissa Tsuboi <narissatsuboi@gmail.com>
2021-09-18 15:59:07 -0700

    Modularize Palindrome LinkedList functions

--- 3 - Fast and Slow Pointers/Palindrome LinkedList (medium)/main.py
+++ 3 - Fast and Slow Pointers/Palindrome LinkedList (medium)/main.py
@@ -3,9 +3,11 @@ Given the head of a Singly LinkedList, write a method to check if the
 LinkedList is a palindrome or not.
 Your algorithm should use constant space and the input LinkedList should
 be in the original form once the algorithm is finished. The algorithm should
-have O(N)O(N) time complexity where ‘N’ is the number of nodes in the
+have O(N) time complexity where ‘N’ is the number of nodes in the
 LinkedList.
 """
+
+import unittest
 
 
 class ListNode:
@@ -14,48 +16,26 @@ class ListNode:
         self.next = next
 
 
-def is_palindrome_linked_list(head):
-    """
-    Story: given a singly linked list, determine if the nodes create a
-    palindrome in constant space and returning the linkedlist the same.
-    Input: Head of linked list
-    Output: True if palindrome
-    Approach:
-    If palindrome, the first half of the list is the reverse of the second
-    half of the list. Slow and fast pointers. O(n) space | O(1) time.
-    :param head:
-    :return: True if palindrome
+# Support Functions
+# Find Middle
+# Reverse
+# Print List
 
+
+def find_middle(node):
     """
-    # 1 Find middle of list
-    slow, fast = head, head
-    orig_head = head
+    Uses slow and fast pointers to find middle of linked list.
+    Advance fast pointer 2x as fast as slow pointer. When fast reaches
+    the end of the list, slow is pointing to the middle.
+    O(n) time | O(1) space
+    :param head: start of search for middle
+    :return: node representing middle
+    """
+    slow, fast = node, node
     while fast is not None and fast.next is not None:
-        slow = slow.next
-        fast = fast.next.next
-    # 2 When middle is found, reverse the second half of the list
-    head_second_half = reverse(slow)
-    # print()
-    # print_list(head)
-    # print_list(head_second_half)
-    # 3 Store head to second half so we can undo the reverse later since the
-    # problem requires us to return the list in original order
-    copy_head_second_half = head_second_half
-    # 4 Compare each element in the list with the node value while they match.
-    # If they match until null -> True.
-    while head is not None and head_second_half is not None:
-        if head.value != head_second_half.value:
-            break  # not palindrome, will return False
-        else:  # continue traversals
-            head = head.next
-            head_second_half = head_second_half.next
-    # 5 we have to reverse the linked list back before we return anything
-    reverse(copy_head_second_half)  # undo step 2
-    print_list(orig_head)
-    # 6 Catch palindrome condition
-    if head is None or head_second_half is None:
-        return True
-    return False
+        slow = slow.next  # 1 pace
+        fast = fast.next.next  # 2 paces
+    return slow
 
 
 def reverse(head):
@@ -80,12 +60,83 @@ def print_list(head):
         current = current.next
 
 
-if __name__ == '__main__':
-    head = ListNode(2)
-    head.next = ListNode(4)
-    head.next.next = ListNode(6)
-    head.next.next.next = ListNode(4)
-    head.next.next.next.next = ListNode(1)
+def is_palindrome(head):
+    """
+    Story: given a singly linked list, determine if the nodes create a
+    palindrome in constant space and returning the linkedlist the same.
+    Input: Head of linked list
+    Output: True if palindrome
+    Approach:
+    If palindrome, the first half of the list is the reverse of the second
+    half of the list. Slow and fast pointers. O(n) space | O(1) time.
+    :param head:
+    :return: True if palindrome
+    """
+    # 1 Find middle of list
+    mid_node = find_middle(head)
+    # 2 When middle is found, reverse the second half of the list
+    head_second_half = reverse(mid_node)
+    # 3 Store head to second half so we can undo the reverse later since the
+    # problem requires us to return the list in original order
+    copy_head_second_half = head_second_half
+    # 4 Compare each element in the list with the node value while they match.
+    # If they match until null -> True.
+    while head is not None and head_second_half is not None:
+        if head.value != head_second_half.value:
+            break  # not palindrome, will return False
+        else:  # continue traversals
+            head = head.next
+            head_second_half = head_second_half.next
+    # 5 we have to reverse the linked list back before we return anything
+    reverse(copy_head_second_half)  # undo step 2
+    # 6 Catch palindrome condition
+    if head is None or head_second_half is None:
+        return True
+    return False
 
-    print()
-    print(is_palindrome_linked_list(head))
+
+class TestPalindromeLinkedList(unittest.TestCase):
+
+    def test_find_middle(self):
+        head = ListNode(2)
+        head.next = ListNode(4)
+        head.next.next = ListNode(6)
+        head.next.next.next = ListNode(4)
+        head.next.next.next.next = ListNode(1)
+        self.assertEqual(head.next.next, find_middle(head))
+
+    def test_reverse(self):
+        head = ListNode(2)
+        head.next = ListNode(4)
+        head.next.next = ListNode(6)
+        head.next.next.next = ListNode(4)
+        head.next.next.next.next = ListNode(1)
+        print_list(head)
+        print()
+        rev_head = reverse(head)
+        print_list(rev_head)
+        print()
+
+    def test_is_palindrome(self):
+        head = ListNode(2)
+        head.next = ListNode(4)
+        head.next.next = ListNode(6)
+        head.next.next.next = ListNode(4)
+        head.next.next.next.next = ListNode(2)
+        self.assertTrue(is_palindrome(head))
+        head = ListNode(2)
+        head.next = ListNode(4)
+        head.next.next = ListNode(6)
+        head.next.next.next = ListNode(4)
+        head.next.next.next.next = ListNode(1)
+        self.assertFalse(is_palindrome(head))
+        head = None
+        self.assertTrue(is_palindrome(head))
+
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity=2)
+
+
+
